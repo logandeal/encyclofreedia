7eb513b5b45a36901a7b9f70ceef37c84c31e579
blaisebisch <blaise.w.e@gmail.com>
2023-11-04 12:38:00 -0500

    Removed explorecontainer, placed tab nav bar in top slot

--- encyclofreedia/src/pages/Search.tsx
+++ encyclofreedia/src/pages/Search.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-
 
 const Search: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Search</IonTitle>
-        </IonToolbar>
+        
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
@@ -17,7 +13,6 @@ const Search: React.FC = () => {
             <IonTitle size="large">Search</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
       </IonContent>
     </IonPage>
   );
