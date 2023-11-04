b4c7f130639810e75a48acf58462be54611b5ddc
Logan Deal <53441579+logandeal@users.noreply.github.com>
2023-11-04 00:31:39 -0500

    add font

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle style={{ fontFamily: "MuseoModerno", fontSize: "30px" }}>
+            Freedia
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">TEST</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
