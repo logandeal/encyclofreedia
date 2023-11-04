18a78d636f7f40232b15b063c72e45794a3c0218
Logan Deal <53441579+logandeal@users.noreply.github.com>
2023-11-04 12:54:32 -0500

    add animation

--- frontend/src/components/TopicGuide.tsx
+++ frontend/src/components/TopicGuide.tsx
@@ -12,6 +12,12 @@ import type { Animation } from "@ionic/react";
 const TopicGuide: React.FC = () => {
   const cardAEl = useRef<HTMLIonCardElement | null>(null);
   const cardBEl = useRef<HTMLIonCardElement | null>(null);
+  const cardCEl = useRef<HTMLIonCardElement | null>(null);
+  const cardDEl = useRef<HTMLIonCardElement | null>(null);
+  const cardEEl = useRef<HTMLIonCardElement | null>(null);
+  const cardFEl = useRef<HTMLIonCardElement | null>(null);
+  const cardGEl = useRef<HTMLIonCardElement | null>(null);
+  const cardHEl = useRef<HTMLIonCardElement | null>(null);
 
   const animation = useRef<Animation | null>(null);
 
@@ -19,26 +25,76 @@ const TopicGuide: React.FC = () => {
     if (animation.current === null) {
       const cardA = createAnimation()
         .addElement(cardAEl.current!)
-        .duration(1500)
+        .duration(21000)
         .iterations(Infinity)
-        .fromTo("transform", "translateX(-500px)", "translateX(100px)")
-        .fromTo("opacity", "1", "0.2");
+        .fromTo("transform", "translateX(-50vw)", "translateX(0vw)")
+        .fromTo("opacity", "1", "0.8");
 
       const cardB = createAnimation()
         .addElement(cardBEl.current!)
-        .duration(1500)
+        .duration(21000)
         .iterations(Infinity)
-        .fromTo("transform", "translateX(0px)", "translateX(100px)")
-        .fromTo("opacity", "1", "0.2");
+        .fromTo("transform", "translateX(-50vw)", "translateX(0vw)")
+        .fromTo("opacity", "1", "0.8");
+
+      const cardC = createAnimation()
+        .addElement(cardCEl.current!)
+        .duration(21000)
+        .iterations(Infinity)
+        .fromTo("transform", "translateX(-50vw)", "translateX(0vw)")
+        .fromTo("opacity", "1", "0.8");
+
+      const cardD = createAnimation()
+        .addElement(cardDEl.current!)
+        .duration(21000)
+        .iterations(Infinity)
+        .fromTo("transform", "translateX(-50vw)", "translateX(0vw)")
+        .fromTo("opacity", "1", "0.8");
+
+      const cardE = createAnimation()
+        .addElement(cardEEl.current!)
+        .duration(21000)
+        .iterations(Infinity)
+        .fromTo("transform", "translateX(-50vw)", "translateX(0vw)")
+        .fromTo("opacity", "1", "0.8");
+
+      const cardF = createAnimation()
+        .addElement(cardFEl.current!)
+        .duration(21000)
+        .iterations(Infinity)
+        .fromTo("transform", "translateX(-50vw)", "translateX(0vw)")
+        .fromTo("opacity", "1", "0.8");
+
+      const cardG = createAnimation()
+        .addElement(cardGEl.current!)
+        .duration(21000)
+        .iterations(Infinity)
+        .fromTo("transform", "translateX(-50vw)", "translateX(0vw)")
+        .fromTo("opacity", "1", "0.8");
+
+      const cardH = createAnimation()
+        .addElement(cardHEl.current!)
+        .duration(21000)
+        .iterations(Infinity)
+        .fromTo("transform", "translateX(-50vw)", "translateX(0vw)")
+        .fromTo("opacity", "1", "0.8");
 
       animation.current = createAnimation()
-        .duration(2000)
+        .duration(21000)
         .iterations(Infinity)
-        .addAnimation([cardA, cardB]);
+        .addAnimation([cardA, cardB, cardC, cardD, cardE, cardF, cardG, cardH]);
     }
-  }, [cardAEl.current, cardBEl.current]);
-
-  animation.current?.play();
+    animation.current?.play();
+  }, [
+    cardAEl.current,
+    cardBEl.current,
+    cardCEl.current,
+    cardDEl.current,
+    cardEEl.current,
+    cardFEl.current,
+    cardGEl.current,
+    cardHEl.current,
+  ]);
 
   return (
     <div
@@ -46,19 +102,61 @@ const TopicGuide: React.FC = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        height: "75vh",
+        height: "65vh",
       }}
     >
+      <IonCard color="tertiary" ref={cardHEl}>
+        <IonCardHeader>
+          <IonCardTitle>Education</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+        <IonCardContent>Card Content</IonCardContent>
+      </IonCard>
+      <IonCard color="success" ref={cardGEl}>
+        <IonCardHeader>
+          <IonCardTitle>Environment</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+        <IonCardContent>Card Content</IonCardContent>
+      </IonCard>
+      <IonCard color="warning" ref={cardFEl}>
+        <IonCardHeader>
+          <IonCardTitle>Organizations</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+        <IonCardContent>Card Content</IonCardContent>
+      </IonCard>
       <IonCard color="primary" ref={cardAEl}>
         <IonCardHeader>
-          <IonCardTitle>Health</IonCardTitle>
+          <IonCardTitle>Housing</IonCardTitle>
           <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
         </IonCardHeader>
         <IonCardContent>Card Content</IonCardContent>
       </IonCard>
       <IonCard color="secondary" ref={cardBEl}>
         <IonCardHeader>
-          <IonCardTitle>Finance</IonCardTitle>
+          <IonCardTitle>Health</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+        <IonCardContent>Card Content</IonCardContent>
+      </IonCard>
+      <IonCard color="tertiary" ref={cardCEl}>
+        <IonCardHeader>
+          <IonCardTitle>Education</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+        <IonCardContent>Card Content</IonCardContent>
+      </IonCard>
+      <IonCard color="success" ref={cardDEl}>
+        <IonCardHeader>
+          <IonCardTitle>Environment</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+        <IonCardContent>Card Content</IonCardContent>
+      </IonCard>
+      <IonCard color="warning" ref={cardEEl}>
+        <IonCardHeader>
+          <IonCardTitle>Organizations</IonCardTitle>
           <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
         </IonCardHeader>
         <IonCardContent>Card Content</IonCardContent>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   IonContent,
   IonHeader,
   IonPage,
   IonTitle,
   IonToolbar,
+  IonToggle,
 } from "@ionic/react";
+import type { ToggleCustomEvent } from "@ionic/react";
 import Heading from "../components/Heading";
 import TopicGuide from "../components/TopicGuide";
 import "./Home.css";
 
 const Home: React.FC = () => {
+  // Add or remove the "dark" class on the document body
+  const toggleDarkTheme = (shouldAdd: boolean) => {
+    document.body.classList.toggle("dark", shouldAdd);
+  };
+
+  useEffect(() => {
+    toggleDarkTheme(true);
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle style={{ fontFamily: "MuseoModerno", fontSize: "30px" }}>
+          <IonTitle
+            style={{
+              fontFamily: "MuseoModerno",
+              fontSize: "30px",
+              float: "left",
+            }}
+          >
             Freedia
           </IonTitle>
         </IonToolbar>
@@ -27,6 +44,19 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <Heading />
+        <p
+          style={{
+            fontFamily: "MuseoModerno",
+            fontSize: "25px",
+            color: "#8c8c8c",
+            position: "absolute",
+            top: "35%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          Many topics to explore...
+        </p>
         <TopicGuide />
       </IonContent>
     </IonPage>
